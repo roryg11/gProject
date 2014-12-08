398cd906d64d1282252163ef26ee3bccaddd2b3b
roryg11 <Rory.C.Grant@gmail.com>
2014-12-08 13:40:09 -0800

    index page and new trip form and page

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -2,4 +2,33 @@ class TripsController < ApplicationController
   def index
     @trips = Trip.all
   end
+
+  def new
+    @trip=Trip.new
+  end
+
+  def create
+    @trip=Trip.new(trip_params)
+    if @trip.save
+      redirect_to trips_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @trip = Trip.find(params[:id])
+  end
+
+  private
+
+  def trip_params
+    params.require(:trip).permit(
+      :name,
+      :date_of_trip,
+      :location,
+      :activities,
+      :trip_type,
+    )
+  end
 end
