610e3f25bba4397b6e7514de3c310c2aef0d06a9
roryg11 <Rory.C.Grant@gmail.com>
2014-12-08 17:52:57 -0800

    adding delete/destroy feature

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -10,8 +10,7 @@ class TripsController < ApplicationController
   def create
     @trip=Trip.new(trip_params)
     if @trip.save
-      redirect_to trips_path
-      flash[:notice] = "Trip successfully created"
+      redirect_to trips_path, notice: "Trip successfully created"
     else
       render :new
     end
@@ -23,6 +22,22 @@ class TripsController < ApplicationController
 
   def edit
     @trip = Trip.find(params[:id])
+  end
+
+  def update
+    @trip = Trip.find(params[:id])
+    @trip.update(trip_params)
+    if @trip.save
+      redirect_to trips_path, notice: "Trip successfully updated"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @trip = Trip.find(params[:id])
+    @trip.destroy
+    redirect_to trips_path, notice: "Trip successfully deleted"
   end
 
   private
